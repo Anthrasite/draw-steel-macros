b9b29916b91c15f2841845d7df4964b2f113e1ac
Anthrasite <Anthrasite@users.noreply.github.com>
2025-08-02 02:27:42 -0500

    Update "Behold the Face of Justice!" to include the extra psychic damage

--- macros/CharacterFeatures/Classes/Class_Censor/1_a5_BeholdTheFaceOfJustice.js
+++ macros/CharacterFeatures/Classes/Class_Censor/1_a5_BeholdTheFaceOfJustice.js
@@ -14,5 +14,22 @@ await game.macros.getName("ShareAbility").execute({
   tier1Effect: "3 + M holy damage; if the target has P < WEAK, each enemy within 2 squares of them is frightened of you (save ends)",
   tier2Effect: "5 + M holy damage; if the target has P < AVERAGE, each enemy within 2 squares of them is frightened of you (save ends)",
   tier3Effect: "8 + M holy damage; if the target has P < STRONG, each enemy within 2 squares of them is frightened of you (save ends)",
-  effect: "Each enemy frightened this way is pushed up to 2 squares away from the target and takes psychic damage equal to your Presence score."
+  effect: "Each enemy frightened this way is pushed up to 2 squares away from the target and takes psychic damage equal to your Presence score.",
+  afterRollFunc: async function() {
+    const isEnemyFrightened = await Dialog.confirm({
+      title: `Enemies frightened?`,
+      content: `<p>Were any enemies frightened by you?</p>`,
+      defaultYes: false
+    });
+
+    if (isEnemyFrightened) {
+      const extraDamage = Math.max(0, await game.macros.getName(`GetCharacteristic`).execute({ activeActor: actor, characteristicName: "Presence" }));
+      const roll = await new Roll(extraDamage.toString()).evaluate();
+      await game.macros.getName(`ShareRoll`).execute({
+        activeActor: actor,
+        roll,
+        flavor: `Psychic damage`
+      });
+    }
+  }
 });
