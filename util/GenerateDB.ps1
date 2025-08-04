336eaf854b270bfb7b74d91ae135dfdad87c4910
Anthrasite <Anthrasite@users.noreply.github.com>
2025-08-04 12:13:42 -0500

    Add an HTML file for each item's description and modify the DB script to create DB files for each set of item descriptions

--- util/GenerateDB.ps1
+++ util/GenerateDB.ps1
@@ -1,15 +1,11 @@
 $packsJson = @()
 $macroIds = @()
 
+# Generate macro DB files
 Get-ChildItem $PSScriptRoot\..\macros -Directory -Recurse | ForEach-Object {
     $dirPath = $_
     $dirName = Split-Path $_ -Leaf
     $dbFileContents = ""
-
-    # Delete all existing db files
-    Get-ChildItem $_ -Filter *.db | ForEach-Object {
-        Remove-Item -Path $_.FullName
-    }
 
     # Add all js files to the new db file
     Get-ChildItem $_ -Filter *.js | ForEach-Object {
@@ -118,7 +114,93 @@ Get-ChildItem $PSScriptRoot\..\macros -Directory -Recurse | ForEach-Object {
             "type" = "Macro"
         }
 
-        New-Item "..\packs\${packDBFileName}" -ItemType File -Force -Value $dbFileContents | Out-Null
+        New-Item "packs\${packDBFileName}" -ItemType File -Force -Value $dbFileContents | Out-Null
+    }
+}
+
+# Generate item DB files
+Get-ChildItem $PSScriptRoot\..\items -Directory -Recurse | ForEach-Object {
+    $dirPath = $_
+    $dirName = Split-Path $_ -Leaf
+    $dbFileContents = ""
+
+    # Add all html files to the new db file
+    Get-ChildItem $_ -Filter *.html | ForEach-Object {
+        if ($_.BaseName.StartsWith("_"))
+            { return }
+
+        $fileContents = Get-Content $_
+        $updateFile = $false
+
+        # Get the existing icon path or use a default icon
+        $imgPath = $null
+        $imgMatches = Select-String -Path $_ '<!-- @img=(.*) -->' -AllMatches
+        if ($imgMatches.Length -gt 0)
+            { $imgPath = $imgMatches[0].Matches[0].Groups[1].Value }
+        else {
+            $imgPath = "icons/svg/dice-target.svg"
+
+            $fileContents = ,"<!-- @img=${imgPath} -->" + $fileContents
+            $updateFile = $true
+        }
+
+        # Get the existing name or use the file name
+        $name = $_.BaseName
+        $nameMatches = Select-String -Path $_ '<!-- @name=(.*) -->' -AllMatches
+        if ($nameMatches.Length -gt 0)
+            { $name = $nameMatches[0].Matches[0].Groups[1].Value }
+        else {
+            $fileContents = ,"<!-- @name=${name} -->" + $fileContents
+            $updateFile = $true
+        }
+
+        # Get the existing id from the file, or generate a new id and prepend it to the file
+        $id = $null
+        $idMatches = Select-String -Path $_ '<!-- @id=(.*) -->' -AllMatches
+        if ($idMatches.Length -gt 0)
+            { $id = $idMatches[0].Matches[0].Groups[1].Value }
+        else {
+            $charSet = [Char[]]'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+            $id = (Get-Random -Count 16 -InputObject $charSet) -join ''
+
+            $fileContents = ,"<!-- @id=${id} -->" + $fileContents
+            $updateFile = $true
+        }
+
+        # Ensure this id doesn't already exist
+        if ($macroIds -contains $id) {
+            Write-Host "Error: Multiple macros with id $id"
+            Exit
+        }
+        $macroIds += $id
+
+        # Get the file contents as a single string, escaping slashes, quotes, and newlines
+        $description = $fileContents | ForEach-Object{
+            $_.replace("\", "\\").replace("`"", "\`"")
+        } | Join-String -Separator "\n"
+
+        $dbFileContents = $dbFileContents + "{ `"_id`": `"${id}`", `"name`": `"${name}`", `"img`": `"${imgPath}`", `"type`": `"item`", `"ownership`": { `"default`": 0 }, `"effects`": [], `"system`": { `"description`": `"${description}`", `"quantity`": 1, `"weight`": 0, `"attributes`": {}, `"groups`": {} } }`n"
+
+        if ($updateFile)
+            { $fileContents | Set-Content $_ }
+    }
+
+    # If the db file isn't empty (i.e. there were html files in this directory), create pack JSON and a db file
+    if ($dbFileContents.length -gt 0) {
+        $packPath = ($_ | Resolve-Path -Relative).substring(2)
+
+        $packName = $dirName
+        $packLabel = $packName.replace("_", " - ")
+        $packDBFileName = "${packName}_items.db"
+
+        $packsJson += @{
+            "name" = $packName
+            "label" = $packLabel
+            "path" = "packs\${packDBFileName}"
+            "type" = "Item"
+        }
+
+        New-Item "packs\${packDBFileName}" -ItemType File -Force -Value $dbFileContents | Out-Null
     }
 }
 
