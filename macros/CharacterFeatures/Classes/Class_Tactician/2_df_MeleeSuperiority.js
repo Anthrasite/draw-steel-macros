a57a5f12695e4f39a7f272efcf0c31d4bb2b4ed7
Anthrasite <Anthrasite@users.noreply.github.com>
2025-11-04 19:37:22 -0600

    Add missing text to Melee Superiority feature macro

--- macros/CharacterFeatures/Classes/Class_Tactician/2_df_MeleeSuperiority.js
+++ macros/CharacterFeatures/Classes/Class_Tactician/2_df_MeleeSuperiority.js
@@ -4,6 +4,7 @@
 await game.macros.getName("ShareFeature").execute({
   activeActor: actor,
   name: "Melee Superiority",
-  description: `After constant drills, you can more accurately anticipate an enemy’s plan and thwart their attempts to move across the battlefield. Whenever you make an opportunity attack, the target’s speed is reduced to 0 until the end of the current turn.`,
+  description: `After constant drills, you can more accurately anticipate an enemy’s plan and thwart their attempts to move across the battlefield. Whenever you make an opportunity attack, the target’s speed is reduced to 0 until the end of the current turn.</p>
+    <p><strong>Mark Benefit:</strong> When a creature marked by you attempts to move or shift within distance of your melee free strike, you can use a free triggered action and spend 2 focus to make a melee free strike against that creature.</p>`,
   source: "Class"
 });
