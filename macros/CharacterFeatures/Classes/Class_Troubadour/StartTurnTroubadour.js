945b455973fd5333c814872a68d9c4c348d96a98
Anthrasite <Anthrasite@users.noreply.github.com>
2026-01-12 02:59:43 -0600

    Add missing macro for Troubadour turn start

--- macros/CharacterFeatures/Classes/Class_Troubadour/StartTurnTroubadour.js
+++ macros/CharacterFeatures/Classes/Class_Troubadour/StartTurnTroubadour.js
@@ -1,4 +1,4 @@
-//@id=h3IOpVPol84Fj571
+//@id=R6tqC2BlkbesXYMK
 //@name=Start turn
 //@img=icons/skills/targeting/crosshair-arrowhead-blue.webp
 await game.macros.getName(`StartTurn`).execute({ activeActor: actor, resourceRoll: `1d3` });
